28ad39680c95c853535d0a839f668d889275043d
Enrique Maldonado <emaldonado@modyo.com>
2023-02-01 10:54:15 -0300

    fix: PersisHandlerConfig
    - Se deja condicional

--- src/main/java/com/modyo/ms/commons/statemachine/config/PersistHandlerConfig.java
+++ src/main/java/com/modyo/ms/commons/statemachine/config/PersistHandlerConfig.java
@@ -1,6 +1,7 @@
 package com.modyo.ms.commons.statemachine.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.StateMachine;
@@ -11,6 +12,7 @@ import org.springframework.statemachine.recipes.persist.PersistStateMachineHandl
 public class PersistHandlerConfig {
 
   @Bean
+  @ConditionalOnBean(PersistStateChangeListener.class)
   public PersistStateMachineHandler persistStateMachineHandler(
       @Autowired StateMachine<String, String> stateMachine,
       @Autowired PersistStateChangeListener persistStateChangeListener) {
